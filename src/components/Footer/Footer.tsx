bb8ee9239a75fdf78d8806104159c14ffa669e80
Johnnybar <johnnybareket@gmail.com>
2022-02-16 14:53:51 +0000

    develop - moves header and footer to separate components

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.scss";
+
+function Footer() {
+  return (
+    <footer className="cosuno-footer">
+      <p className="cosuno-footer__text text-center">Cosuno Footer</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header.scss";
+
+function Header() {
+  return (
+    <header className="cosuno-header">
+      <img src="" alt="cosuno logo" className="cosuno-header__logo" />
+      <h3 className="cosuno-header__heading  text-center ">
+        Company live search and filter
+      </h3>
+    </header>
+  );
+}
+
+export default Header;

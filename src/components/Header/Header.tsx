2f4b2b938b2d44e9753e82b7b443a9aed963ee78
Johnnybar <johnnybareket@gmail.com>
2022-02-16 16:53:26 +0000

    develop - refactors Header class names, layout and css, adds logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import "./Header.scss";
+import logo from "../../assets/cosuno-logo.svg";
 
 function Header() {
   return (
-    <header className="cosuno-header">
-      <img src="" alt="cosuno logo" className="cosuno-header__logo" />
-      <h3 className="cosuno-header__heading  text-center ">
-        Company live search and filter
-      </h3>
+    <header className="cosuno-header container">
+      <div className="row">
+        <div className="col-12">
+          <div className="cosuno-header__content-wrapper">
+            <img src={logo} alt="cosuno logo" className="cosuno-header__logo" />
+            <h3 className="cosuno-header__heading  text-center ">
+              Company live search and filter
+            </h3>
+          </div>
+        </div>
+        <div></div>
+      </div>
     </header>
   );
 }

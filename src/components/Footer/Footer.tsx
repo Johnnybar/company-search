5b15402247347c4ac1f741e5587916665a52810e
Johnnybar <johnnybareket@gmail.com>
2022-02-16 17:11:25 +0000

    develop - fixes footer min-height, modifies footer text

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,7 +4,9 @@ import "./Footer.scss";
 function Footer() {
   return (
     <footer className="cosuno-footer">
-      <p className="cosuno-footer__text text-center">Cosuno Footer</p>
+      <p className="cosuno-footer__text text-center">
+        Cosuno - Subcontractor management simply and intelligently solved
+      </p>
     </footer>
   );
 }
